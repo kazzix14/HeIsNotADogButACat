87f91c45d04863ed58f46db4a82fc8fecb667e5d
Kazuma <atugari27@gmail.com>
2018-12-12 15:39:04 +0000

    reform

---
+++ clock/include/vector4d.h
@@ -0,0 +1,22 @@
+#ifndef VECTOR4D_H
+#define VECTOR4D_H
+
+typedef struct vector4d
+{
+	int x,
+	    y,
+	    z,
+	    w;
+} Vector4D;
+
+void Vector4D_set(Vector4D* const, const Vector4D*);
+void Vector4D_set_zero(Vector4D* const);
+void Vector4D_set_identity(Vector4D* const);
+void Vector4D_set_unit_x(Vector4D* const);
+void Vector4D_set_unit_y(Vector4D* const);
+void Vector4D_set_unit_z(Vector4D* const);
+void Vector4D_set_unit_w(Vector4D* const);
+void Vector4D_add(Vector4D* const, const Vector4D*);
+void Vector4D_sub(Vector4D* const, const Vector4D*);
+
+#endif
---
+++ clock/src/vector3d.c
@@ -0,0 +1,59 @@
+#define GLUT_DISABLE_ATEXIT_HACK
+
+#include "vector3d.h"
+
+void Vector3D_set(Vector3D* const p_this, const Vector3D* p_pos)
+{
+	p_this->x = p_pos->x;
+	p_this->y = p_pos->y;
+	p_this->z = p_pos->z;
+}
+
+void Vector3D_set_zero(Vector3D* const p_this)
+{
+	p_this->x = 0;
+	p_this->y = 0;
+	p_this->z = 0;
+}
+
+void Vector3D_set_identity(Vector3D* const p_this)
+{
+	p_this->x = 1;
+	p_this->y = 1;
+	p_this->z = 1;
+}
+
+void Vector3D_set_unit_x(Vector3D* const p_this)
+{
+	p_this->x = 1;
+	p_this->y = 0;
+	p_this->z = 0;
+}
+
+void Vector3D_set_unit_y(Vector3D* const p_this)
+{
+	p_this->x = 0;
+	p_this->y = 1;
+	p_this->z = 0;
+}
+
+void Vector3D_set_unit_z(Vector3D* const p_this)
+{
+	p_this->x = 0;
+	p_this->y = 0;
+	p_this->z = 1;
+}
+
+void Vector3D_add(Vector3D* const p_this, const Vector3D* p_oprnd)
+{
+	p_this->x += p_oprnd->x;
+	p_this->y += p_oprnd->y;
+	p_this->z += p_oprnd->z;
+}
+
+void Vector3D_sub(Vector3D* const p_this, const Vector3D* p_oprnd)
+{
+	p_this->x -= p_oprnd->x;
+	p_this->y -= p_oprnd->y;
+	p_this->z -= p_oprnd->z;
+}
